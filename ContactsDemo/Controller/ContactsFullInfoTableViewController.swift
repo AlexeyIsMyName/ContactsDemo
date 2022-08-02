5ba024b9eb9778f23244614b5a607d28e3590705
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-02 19:02:05 +0300

    Updated ContactsFullInfoTableVC

--- ContactsDemo/Controller/ContactsFullInfoTableViewController.swift
+++ ContactsDemo/Controller/ContactsFullInfoTableViewController.swift
@@ -12,23 +12,26 @@ class ContactsFullInfoTableViewController: UITableViewController {
     var contacts: [Person]!
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         return contacts.count
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        contacts[section].fullName
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "section")
-        let person = contacts[section]
-        var content = cell.defaultContentConfiguration()
-        content.text = person.fullName
-        cell.contentConfiguration = content
-        return cell
-    }
+//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "section")
+//        let person = contacts[section]
+//        var content = cell.defaultContentConfiguration()
+//        content.text = person.fullName
+//        cell.contentConfiguration = content
+//        return cell
+//    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
@@ -47,7 +50,11 @@ class ContactsFullInfoTableViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        view.backgroundColor = .systemGray5
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+//    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+//        view.backgroundColor = .systemGray5
+//    }
 }
