83d9fc2a33aedbe88f6eef4286dece2c3c779334
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-02 18:54:12 +0300

    Updated ContactsFullNameTableVC

--- ContactsDemo/Controller/ContactsFullNameTableViewController.swift
+++ ContactsDemo/Controller/ContactsFullNameTableViewController.swift
@@ -12,20 +12,22 @@ class ContactsFullNameTableViewController: UITableViewController {
     var contacts: [Person]!
 
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "fullNameCell", for: indexPath)
-        let person = contacts[indexPath.row]
         var content = cell.defaultContentConfiguration()
+        let person = contacts[indexPath.row]
+        
         content.text = person.fullName
         cell.contentConfiguration = content
+        
         return cell
     }
     
+    // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let detailsContactVC = segue.destination as? DetailsContactViewController else { return }
         guard let index = tableView.indexPathForSelectedRow?.row else { return }
