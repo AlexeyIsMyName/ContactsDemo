8eadbdc4b31ebba022f2bb4469df43de4cd2b400
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-02 15:36:35 +0300

    Added TabBarController

--- ContactsDemo/Controller/TabBarController.swift
+++ ContactsDemo/Controller/TabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarController.swift
 //  ContactsDemo
 //
 //  Created by ALEKSEY SUSLOV on 02.08.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
